9d7c89e2c33ea297e09da5e81e1151afd896ad26
y77cao <yujiac8000@gmail.com>
2023-04-03 00:04:09 -0400

    changed my mind

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,6 @@
 export const gameContainerElementId = "game-container";
 
 export enum SceneKeys {
-  Bootstrap = "bootstrap",
-  Preload = "preload",
   Game = "game",
-  GameUI = "game-ui",
   GameOver = "game-over",
-  TipsInterstitial = "tip-interstitial",
 }
